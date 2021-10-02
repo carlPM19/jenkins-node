cea6f4842321f96a5c3a88fd6ced2d0f555cf873
carlos <carlosspirir19@gmail.com>
2021-10-02 14:42:37 -0600

    Cambios de archivo

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Docker-Meetup</h1>
+          <h1 className="App-title">Curso Sistemas Operativos 1</h1>
         </header>
         <p className="App-intro">
-          Welcome to this Docker Meetup with Jenkins
+          Universidad Mariano Galvez de Guatemala, Curso SO1, Carlos Pirir
 
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
